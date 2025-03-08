e5cd576196ffd3f8ffe50ccd721a0ec7c01a1c2a
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2025-03-08 09:42:32 +0530

    setup server
    setup server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+// mongodb+srv://admin:admin123@cluster0.ocncl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const express = require('express')
+
+const app = express;
+
+app.listen(3000, () => {
+    console.log("app is running");
+})
