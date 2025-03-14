c2b1322bc24e57efe7b0981d98a539ed54bfc114
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2025-03-14 12:01:42 +0530

    Update UserModel.js
    with login details

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -3,8 +3,16 @@ const Schema = mongoose.Schema;
 
 const userScema = new Schema({
 
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ['ADMIN', 'AGENT', 'CUSTOMER'],
+        default: 'CUSTOMER'
+    },
 
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model(
     "UserModel", //file name
